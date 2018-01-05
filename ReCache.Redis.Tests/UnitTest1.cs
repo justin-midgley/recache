b660423c3311967021807cd1cdfdd6244ef19940
Justin Midgley <midgley.justin@gmail.com>
2018-01-05 15:13:15 +0300

    Add Redis unit test project

---
+++ ReCache.Redis.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReCache.Redis.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
